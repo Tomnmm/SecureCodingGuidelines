f5ccfd7a98b64a011fd353a7c9e76dd7d582ad1d
Jurgen Taverniers <jurgen.taverniers@student.howest.be>
2017-10-12 18:21:30 +0200

    Changes jtaverniers

---
+++ guidelines/g0_Fundamentals/g0_06/src/be.howest.ti.secure.development.g0.g06/domain/SafeCarpass.java
@@ -0,0 +1,29 @@
+package be.howest.ti.secure.development.g0.g06.domain;
+
+import java.math.BigDecimal;
+
+public class SafeCarpass {
+    private String vinNumber;
+    private BigDecimal mileage;
+
+    public SafeCarpass(String vinNumber, BigDecimal mileage){
+        this.vinNumber = vinNumber;
+        this.mileage = mileage;
+    }
+
+    public String getVinNumber(){
+        return  vinNumber;
+    }
+
+    public BigDecimal getMileage(){
+        return mileage;
+    }
+
+    @Override
+    public String toString() {
+        return "Carpass{" +
+                "vinNumber='" + vinNumber + '\'' +
+                ", mileage=" + mileage +
+                '}';
+    }
+}
