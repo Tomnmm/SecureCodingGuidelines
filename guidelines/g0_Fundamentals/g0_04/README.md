d58fa14d0958dd8b3a50de06179388e61c372945
Manu De Witte <emmanuel.de.witte@student.howest.be>
2017-10-20 15:22:22 +0200

    g0_04 example - establish trust boundaries

--- guidelines/g0_Fundamentals/g0_04/README.md
+++ guidelines/g0_Fundamentals/g0_04/README.md
@@ -9,3 +9,13 @@ In order to ensure that a system is protected, it is necessary to establish trus
 For instance, a web browser is outside of the system for a web server. Equally, a web server is outside of the system for a web browser. Therefore, web browser and server software should not rely upon the behavior of the other for security.
 
 When auditing trust boundaries, there are some questions that should be kept in mind. Are the code and data used sufficiently trusted? Could a library be replaced with a malicious implementation? Is untrusted configuration data being used? Is code calling with lower privileges adequately protected against?
+
+# Don't let untrusted code change your private fields
+![Author](https://img.shields.io/badge/Author-Manu.DeWitte-blue.svg)
+![Date](https://img.shields.io/badge/Date-20171020-lightgrey.svg)
+![Agree](https://img.shields.io/badge/AGREE-0-green.svg)
+![Disagree](https://img.shields.io/badge/DISAGREE-0-red.svg)
+
+For the Java fans. If your security manager does not limit the use of reflection, untrusted third-party code may be able 
+to change your private fields. Class be.howest.ti.secure.development.g0.g04.OnePlusOneEqualsFour shows how you change 
+the internal value of an Integer object.
---
+++ guidelines/g0_Fundamentals/g0_04/src/be/howest/ti/secure/development/g0/g04/OnePlusOneEqualsFour.java
@@ -0,0 +1,42 @@
+package be.howest.ti.secure.development.g0.g04;
+
+import java.lang.reflect.Field;
+
+/**
+ * Shows how private fields are not safe when you're using untrusted third-party code and your JVM
+ * does not limit the use of reflection.
+ */
+public class OnePlusOneEqualsFour {
+
+    public static void main(String[] args) throws Exception {
+        Integer one = 1;
+
+        new MaliciousThirdPartyClass().naughtyCall();
+
+        // 1 + 1 equals 2, right?
+        System.out.println("1 + 1 = " + (one + one));
+    }
+
+    private static class MaliciousThirdPartyClass {
+        private void naughtyCall() {
+            Integer i = 1; // JVM applies autoboxing and will reuse the Integer object assigned to variable "one"
+
+            Class clazz = i.getClass();
+            Field field;
+
+            // the naughty stuff: change the internal value of the Integer object...
+            try {
+                field = clazz.getDeclaredField("value");
+                field.setAccessible(true);
+                field.set(i, 2); // let's give it the value 2
+
+                System.out.println("Ouch, Integer private field \"value\" was updated from value 1 to 2. "
+                        + "You're vulnerable.");
+            } catch (NoSuchFieldException e) {
+                System.out.println("Oops, Integer private field \"value\" not found. You're safe.");
+            } catch (IllegalAccessException e) {
+                System.out.println("Oops, access to Integer private field \"value\" not allowed. You're safe.");
+            }
+        }
+    }
+}
