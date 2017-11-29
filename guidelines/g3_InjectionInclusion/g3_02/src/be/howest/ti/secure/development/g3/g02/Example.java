bbe1557a374bdbb6356fc9185c109ff73bdd7ef7
Robin Peiremans <robin.peiremans@student.howest.be>
2017-11-29 21:38:33 +0100

    Added g3_02 example

---
+++ guidelines/g3_InjectionInclusion/g3_02/src/be/howest/ti/secure/development/g3/g02/Example.java
@@ -0,0 +1,70 @@
+package be.howest.ti.secure.development.g3.g02;
+
+import java.sql.*;
+
+
+public class Example {
+    public static void main(String[] args) {
+        new Example().run();
+    }
+
+    public void run() {
+        Connection db = null;
+        String user_input = "' or 1=1--";
+        String dbFile = "/tmp/example.sqlite";          // Modify this to match your environment
+
+        try {
+            System.out.println("Connect to the database");
+            String url = "jdbc:sqlite:" + dbFile;
+            db = DriverManager.getConnection(url);
+            Statement statement = db.createStatement();
+
+            // Set up the database
+            System.out.println("Set up tables");
+            statement.executeUpdate("drop table if exists t1");
+            statement.executeUpdate("create table t1 (id integer, user string, password string)");
+            statement.executeUpdate("insert into t1 values(1, 'test', 'test')");
+
+            // Bad example
+            System.out.println("");
+            System.out.println("Bad example");
+
+            String sql = "select * from t1 where password = '" + user_input + "'";
+            System.out.println(sql);
+            ResultSet rs = statement.executeQuery(sql);
+
+            while(rs.next()) {
+                System.out.println("name = " + rs.getString("user"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+
+            // Good example
+            System.out.println("");
+            System.out.println("Good example");
+
+            sql = "select * from t1 where password = ?";
+            System.out.println(sql);
+            PreparedStatement stmt = db.prepareStatement(sql);
+
+            stmt.setString(1, user_input);
+            rs = stmt.executeQuery();
+
+            while(rs.next()) {
+                System.out.println("name = " + rs.getString("user"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (db != null) {
+                    db.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
+
